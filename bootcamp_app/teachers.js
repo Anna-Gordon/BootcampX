8d2014a3557287abe84f3f5e933d548a5801f0cb
Anna Gordon <annetta@email.com>
2020-03-05 01:26:56 +0000

    Parameterized Query.Malicious input from users

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'vagrant'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+// const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [cohortName];
+let queryString =`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
